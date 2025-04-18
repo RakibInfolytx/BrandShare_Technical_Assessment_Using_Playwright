04e783b70fc3f721e7e99db4afc6dc0e5f2c0f96
Rakib Hasan <rakibul.hasan@infolytx.com>
2025-04-18 10:07:57 +0600

    login script for part one added

---
+++ tests/part-1.spec.js
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test';
+import LoginPage from '../pages/LoginPage.js';
+import AssetsPage from '../pages/AssetsPage.js';
+import credentials from '../data/credentials.json';
+import path from 'path';
+
+
+test.describe('Step 1', () => {
+  let loginPage;
+  let assetsPage;
+
+  const imagePath = path.resolve('data/20.jpg');
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    assetsPage = new AssetsPage(page);
+
+    await page.goto('/');
+    await loginPage.TypeEmail(credentials.username);
+    await loginPage.ClickNext();
+    await loginPage.TypePassword(credentials.password);
+    await loginPage.ClicLogin();
+
+  });
+
+  test('- Should be able to navigate to the right URL', async ({ page }) => {
+    await expect(page).toHaveTitle('Login - SP | Brandshare DAM');
+  });
+
+  test('- Should be able to upload an image', async () => {
+    const uploadedFileName = '20.jpg';
+    const expectedTitle = 'QA Automation Engineer';
+    const expectedType = 'Image';
+
+    await assetsPage.navigateToAssets();
+    await assetsPage.uploadFile(imagePath);
+    await assetsPage.typeTitle(expectedTitle);
+    await assetsPage.selectType(expectedType);
+    await assetsPage.saveUploadedImage();
+    await assetsPage.verifyUpload(uploadedFileName, expectedTitle, expectedType);
+  });
+});
